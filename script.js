2055fdd46599005e3a39a3aaccc79eb4e8dca641
Sinclair <85627923+Debby-arch@users.noreply.github.com>
2024-03-15 00:37:02 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+<script>
+        function validateForm() {
+ 
+            var username = document.getElementById('username').value;
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+            var confirmPassword = document.getElementById('confirmPassword').value;
+            var errorMessage = document.getElementById('errorMessage');
+
+            if (password !== confirmPassword) {
+                errorMessage.textContent = 'Passwords do not match.'for (let index = 0; index < array.length; index++) {
+                    const element = array[index];
+                    ;
+                }
+        &rbrace; 
+            else {
+                    // Perform additional validation if needed
+
+                // Clear any previous error message
+                errorMessage.textContent = '';
+
+                // Submit the form (in a real scenario, you would send data to a server)
+                alert('Sign up successful!');
+            &rbrace;
+        &rbrace;
+    </script>
